d21e179528f4a24ff3071029030c32c1bbd2c89a
99002678 <71488043+99002678@users.noreply.github.com>
2020-10-23 09:11:57 +0530

    Add files via upload

---
+++ BookApp/src/com/bookapp/dao/BookDAOImpl.java
@@ -0,0 +1,54 @@
+package com.bookapp.dao;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.bookapp.model.Book;
+
+public class BookDAOImpl implements BookDAO {
+
+	@Override
+	public List<Book> getAllBooks() {
+		return showBookList();
+	}
+
+	@Override
+	public List<Book> getByAuthor(String author) {
+		List<Book> newBookList = new ArrayList<>();
+		for (Book book : showBookList()) {
+			if(book.getAuthor().equals(author))
+				newBookList.add(book);
+		}
+		return newBookList;
+				
+	}
+
+	@Override
+	public List<Book> getByCategory(String category) {
+		List<Book> newBookList = new ArrayList<>();
+		for (Book book : showBookList()) {
+			if(book.getCategory().equals(category))
+				newBookList.add(book);
+		}
+		return newBookList;
+	}
+
+	@Override
+	public Book getById(int id) {
+		// TODO Auto-generated method stub
+		for (Book book : showBookList()) {
+			if(book.getBookId()==id)
+				return book;
+		}
+		return null;
+	}
+	
+	private List<Book> showBookList(){
+		return Arrays.asList(
+				new Book(11, "Learn Java", "Tech", "Kathy", 900.0),
+				new Book(12, "7 Habits", "SelfHelp", "Steve", 1900.0),
+				new Book(13, "Ikigai", "SelfHelp", "Kathy", 1700.0));
+	}
+
+}
